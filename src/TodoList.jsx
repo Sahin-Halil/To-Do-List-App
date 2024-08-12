281fbb5b12c25c4a7362bbb21ba49f845e40b542
“Sahin” <sahinhalil1311@gmail.com>
2024-08-12 21:16:57 +0100

    Updated the input, i added js code which has two states, first is the input value which updates to whatever the user types, the other is a setter function which changes the value of the first state. In the input tag, the value of the textbar is set to inputValue and OnChange is set to handleInputChange function which calls the second state setInputValue to update the first state inputValue.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 function ToDoList(){
+    const [inputValue, setInputValue] = useState('');
+
+    function handleInputChange(event){
+        setInputValue(event.target.value);
+    }   
+    console.log(inputValue);
     return (
         <div>
             <h1>My To Do List</h1>
-            <input type="text" placeholder="Enter a task" />
-            <button>Add</button>
+            <input type="text" value={inputValue} onChange={handleInputChange} />
         </div>
     )
 }
